c66f90809584cbec4382b0d1429088aba6a7bea4
davide-amabile <davideamabile81@gmail.com>
2020-08-26 14:48:02 +0200

    fizzbuzz

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//1. creare lista da 1 a 100
+for ( var numero = 1; numero <= 100; numero++ ) {
+
+  //2. se il numero e divisibile per 3 allora stampo Fizz
+  if (numero % 3 === 0) {
+    document.getElementById("lista").innerHTML += "<li>" + "Fizz"; + "</li>";
+  }
+  // 3.se il numero è divisibile per 5 allora stampo Buzz
+  else if (numero % 5 === 0){
+    document.getElementById("lista").innerHTML += "<li>" + "Buzz"; + "</li>";
+  }
+  //4. i numeri restanti stampo FizzBuzz
+  else {
+    document.getElementById("lista").innerHTML += "<li>" + "FizzBuzz"; + "</li>";
+  }
+}
